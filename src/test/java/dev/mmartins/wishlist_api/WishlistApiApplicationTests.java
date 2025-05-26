3ee1937911f625348db745f9e61a0664c95bb0a5
Matheus Martins <mail@mmartins.dev>
2025-05-26 19:10:22 -0300

    first commit

---
+++ src/test/java/dev/mmartins/wishlist_api/WishlistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mmartins.wishlist_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
